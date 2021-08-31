c6d44989288d04c7baa3841d9565bc9f62dbeea8
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-01 00:12:09 +0900

    header

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,11 @@ import Logo from "./UI/Logo";
 
 function Header() {
   return (
-    <header className="flex">
+    <header className="flex items-center justify-between py-9 px-10 md:px-40">
       <div>
         <Logo />
       </div>
-      <nav>
+      <nav className="flex items-center space-x-10 justify-items-end text-base font-extralight md:space-x-20">
         <p>ログイン</p>
         <p>会員登録</p>
       </nav>
