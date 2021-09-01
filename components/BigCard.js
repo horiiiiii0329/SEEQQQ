53da632ea871025901ec9a3b67ef40872362cbca
horiiiiii0329 <85778704+horiiiiii0329@users.noreply.github.com>
2021-09-01 19:07:32 +0900

    Bigcard

---
+++ components/BigCard.js
@@ -0,0 +1,14 @@
+function BigCard() {
+  return (
+    <section>
+      <div>
+        <div></div>
+        <div>
+          <h2></h2>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default BigCard;
